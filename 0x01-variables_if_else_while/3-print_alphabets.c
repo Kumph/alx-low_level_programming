99ea98868f502a8e0e5eeb6aa402f59842dfd5d6
Kumph <mphatsokumwendaec@gmail.com>
2023-02-17 14:11:26 -0800

    Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,10 +10,14 @@ int main(void)
 {
 	char M;
 
-        for (M = 'a'; M <= 'Z'; M++)
+        for (M = 'a'; M <= 'z'; M++)
         {
                 putchar(M);
         }
+	for (M = 'A'; M <= 'Z'; M++)
+	{
+		putchar(M);
+	}
         putchar('\n');
         return (0);
 }

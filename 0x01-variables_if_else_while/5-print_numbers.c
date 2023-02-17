ba494551331a495082680faad1ec71ba4c53935d
Kumph <mphatsokumwendaec@gmail.com>
2023-02-17 15:20:03 -0800

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point prit base 10 number
+ *
+ * Return: always 0 (Sucess)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}

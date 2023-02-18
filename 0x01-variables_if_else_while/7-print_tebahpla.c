34bc83fdb0cd5567359e204b11f43d568dd05025
Kumph <mphatsokumwendaec@gmail.com>
2023-02-17 16:11:47 -0800

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio>
+
+/**
+ * main - entry point reverse alphabet
+ *
+ * Return : alwys 0 (sucess)
+ */
+int main(void)
+{
+	char H;
+
+	for (H = 'z'; H >= 'a'; H++)
+	{
+		putchar(H);
+	}
+	putchar('\n');
+	return (0);
+}

0d1873a0570f56244acd28b2317104f333426b1b
Kumph <mphatsokumwendaec@gmail.com>
2023-02-21 14:51:37 -0800

    alphabet printing loop

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+/**
+ * print_alphabet_x10 - loops the print alphabet function
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char m;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (m = 'a'; m <= 'z'; m++)
+		{
+			_putchar(m);
+		}
+		_putchar('\n');
+		return (0);
+	}
+}

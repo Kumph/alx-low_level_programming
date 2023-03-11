994d766bbc70b14c9704eeba854fdb58a6270262
Kumph <mphatsokumwendaec@gmail.com>
2023-03-10 19:09:22 -0800

    checks corrections

--- 0x09-static_libraries/_atoi.c
+++ 0x09-static_libraries/_atoi.c
@@ -1,12 +1,28 @@
 #include "main.h"
-
+#include <bits/stdc++.h>
+using namespace std;
 
 /**
  * _atoi - not sure what this function does
  * @s: input char
  * Return: an int
  */
-int _atoi(char *s)
+ 
+// A simple atoi() function
+int _atoi(char* str)
 {
-	return (0);
+    // Initialize result
+    int res = 0;
+ 
+    // Iterate through all characters
+    // of input string and update result
+    // take ASCII character of corresponding digit and
+    // subtract the code from '0' to get numerical
+    // value and multiply res by 10 to shuffle
+    // digits left to update running total
+    for (int i = 0; str[i] != '\0'; ++i)
+        res = res * 10 + str[i] - '0';
+
+   return (res); 
 }
+    // return result.

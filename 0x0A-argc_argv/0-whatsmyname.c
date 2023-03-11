6ff9f0b4a38a28fed4f85da32db4a0d45f7d2937
Kumph <mphatsokumwendaec@gmail.com>
2023-03-11 04:55:34 -0800

    adding in main.h

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include "main.h"
 
 /**main - prints program name followed by a new line
  * @argc: count of arguments
  * @argv: array of arguments
- * Rturns: name of program
+ * Returns: Alwats 0.
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {

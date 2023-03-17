3d7597a26a5814e06fc0295e47d4a9b062042da9
Kumph <mphatsokumwendaec@gmail.com>
2023-03-17 14:46:37 -0700

    Task 3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -10,29 +10,25 @@
  */
 int *array_range(int min, int max)
 {
-	int a = 0, b = 0, i = 0;
-	int *ptr;
-
+        int a, i = 0;
+        int *ptr;
+	
 	if (min > max)
 	{
 		return (NULL);
 	}
-	else
-	{
-		a = 0;
-		while (min <= max)
-			a++;
-	}
-	ptr = malloc(sizeof(int) * a);
+	
+	a = min - max + 1;
 
+	ptr = malloc(sizeof(int) * a);
+	
 	if (!ptr)
 		return (NULL);
-
-	for (b = min; b <= max; b++)
+	
+	for (i = 0; min <= max; i++)
 	{
-		ptr[i] = b;
-		i++;
-		b++;
+		ptr[i] = min;
+		min++;
 	}
 	return (ptr);
 }

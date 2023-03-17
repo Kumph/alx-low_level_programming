b17ee6ed2991d195f7a036afce90abb872ac2401
Kumph <mphatsokumwendaec@gmail.com>
2023-03-17 10:28:24 -0700

    Task 0 new directory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *malloc_checked - allocates memory using malloc
+ * @b:bytes to allocate
+ * Return: pointer to allocated memory on sucess
+ * returns 98 if malloc fails and terminates normal process
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *c_mall;
+
+	c_mall = malloc(b);
+
+	if (c_mall == NULL)
+		exit (98);
+
+	return (c_mall);
+}

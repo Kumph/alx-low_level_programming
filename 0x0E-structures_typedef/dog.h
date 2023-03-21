8b64bf8e4e351db7aaeddeabadf59f46b0df773c
Kumph <mphatsokumwendaec@gmail.com>
2023-03-20 20:38:14 -0700

    Tasks 2,3,4,5

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,12 +14,12 @@ struct dog
 	char *owner;
 };
 
-typedef struct dog dogo_t;
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dogo_t *new_dog(char *name, float age, char *owner);
-void free_dog(dogo_t *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 char *_strncpy(char *dest, char *src, int n);
 int _strlen(char *s);
 

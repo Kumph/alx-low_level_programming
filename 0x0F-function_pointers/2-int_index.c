2d86c28d707a1e80a7d277cc51c909d37a7a37f3
Kumph <mphatsokumwendaec@gmail.com>
2023-03-23 08:43:48 -0700

    Fixing Betty error return typo

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -5,7 +5,7 @@
  * @array: array of values to be searched
  * @size: size of the array
  * @cmp:  pointer to the function to be used to compare values
- * Returns: the index of the 1st element for which cmp does not return 0
+ * Return: the index of the 1st element for which cmp does not return 0
  * If size <= 0 or no element matches return -1
  */
 int int_index(int *array, int size, int (*cmp)(int))

c21c6d6d506ebf5f40d0a75db0b994ee63ea40c8
Kumph <mphatsokumwendaec@gmail.com>
2023-03-31 12:16:19 -0700

    fix betty errors

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,7 +2,7 @@
 
 /**
  * add_node - function that adds a node to the beginning of a list list_t
- * @h: pointer to the next node in the list
+ * @head: pointer to the first  node in the list
  * @str: data value in the list
  * Return: the address of the new element, or NULL if it failed
  * print [0] (nil) if str is NULL
@@ -19,11 +19,11 @@ list_t *add_node(list_t **head, const char *str)
 	new_item->len = strlen(str);
 	new_item->next = *head;
 	*head = new_item;
-	
+
 	if (new_item->str == NULL)
-        {
+	{
 		return (NULL);
-        }
-	
+	}
+
 	return (new_item);
 }

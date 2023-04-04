4c5b1920dc9e55f03fc211428996f6c00bec3682
Kumph <mphatsokumwendaec@gmail.com>
2023-04-04 04:04:08 -0700

    Task 0

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,9 +3,10 @@
 void print_tort(void) __attribute__ ((constructor));
 
 /**
- * print_tort: prints a given qoute before executing main
+ * print_tort - prints a given qoute before executing main
  */
 void print_tort(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }

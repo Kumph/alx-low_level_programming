64723bbe783aa62e48785c14734d4ca940f60d7c
Kumph <mphatsokumwendaec@gmail.com>
2023-05-08 00:54:41 -0700

    Task 2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: name of the file to append to
+ * @text_content: string to append to file
+ * Return: 1 on sucess -1 on failure of if filename in NULL
+ * If text_content NULL do nothing. Return 1 if file exists
+ * return -1 if file does not exist or do not have required permissions
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	int num_letters;
+	int wr;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		for (num_letters = 0; text_content[num_letters]; num_letters++)
+			;
+
+		wr = write(fd, text_content, num_letters);
+
+		if (wr == -1)
+			return (-1);
+	}
+
+	close(fd);
+
+	return (1);
+}

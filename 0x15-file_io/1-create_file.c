9f09bf139a6e53c3a9af0bc787214c58170d4cc1
Kumph <mphatsokumwendaec@gmail.com>
2023-05-08 00:28:19 -0700

    Task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * create_file - function that creates a file
+ * @filename: name of file to be created
+ * @text_content: Null terminated string to write to created file
+ * Return: 1 on sucess -1 on failure
+ * return -1 if filename is NULL
+ * create empty file if text_content is NULL
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int num_letters;
+	int wr;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	for (num_letters = 0; text_content[num_letters]; num_letters++)
+		;
+
+	wr = write(fd, text_content, num_letters);
+
+	if (wr == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
